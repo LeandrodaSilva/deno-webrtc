ee9c836bf63166c046040109f6b145a85f4b539d
LEANDRO DA SILVA <leandroda.silva@sebsa.com.br>
2025-08-02 13:51:48 -0300

    fix: Remove localAudio

--- public/script.js
+++ public/script.js
@@ -12,21 +12,20 @@ const peer = new Peer(
 
 window.peer = peer;
 
+function getLocalStream() {
+    navigator.mediaDevices
+        .getUserMedia({ video: false, audio: true })
+        .then((stream) => {
+            window.localStream = stream; // A
+            window.localAudio.srcObject = stream; // B
+            window.localAudio.autoplay = true; // C
+        })
+        .catch((err) => {
+            console.error(`you got an error: ${err}`);
+        });
+}
 
-// function getLocalStream() {
-//     navigator.mediaDevices
-//         .getUserMedia({ video: false, audio: true })
-//         .then((stream) => {
-//             window.localStream = stream; // A
-//             // window.localAudio.srcObject = stream; // B
-//             // window.localAudio.autoplay = true; // C
-//         })
-//         .catch((err) => {
-//             console.error(`you got an error: ${err}`);
-//         });
-// }
-//
-// getLocalStream();
+getLocalStream();
 
 peer.on("open", () => {
     document.getElementById("cast-status").textContent =
